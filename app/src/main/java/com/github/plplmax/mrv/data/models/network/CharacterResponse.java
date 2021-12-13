94f34e6162665b9be8fe7cb3f0d54879528b07bf
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2021-12-13 15:31:43 +0300

    MRV-024: Create POJOs for network

---
+++ app/src/main/java/com/github/plplmax/mrv/data/models/network/CharacterResponse.java
@@ -0,0 +1,8 @@
+package com.github.plplmax.mrv.data.models.network;
+
+public class CharacterResponse {
+    public int id;
+    public String name;
+    public String description;
+    public ImageResponse thumbnail;
+}
