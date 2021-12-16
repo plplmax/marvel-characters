1dcab339aa69b6af21b7bc43f5fbfc4aa22f6139
plplmax <50287455+plplmax@users.noreply.github.com>
2021-12-16 22:05:25 +0300

    MRV-028: Add constructors to domain models (#16)

--- app/src/main/java/com/github/plplmax/mrv/domain/models/Image.java
+++ app/src/main/java/com/github/plplmax/mrv/domain/models/Image.java
@@ -4,6 +4,11 @@ public class Image {
     private String path;
     private String extension;
 
+    public Image(String path, String extension) {
+        this.path = path;
+        this.extension = extension;
+    }
+
     public String getPath() {
         return path;
     }
