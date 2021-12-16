62f6a917cb83df1b7c77baaee9abe7e9b82fffbb
plplmax <50287455+plplmax@users.noreply.github.com>
2021-12-16 21:16:01 +0300

    MRV-025: Create Character and Image models (#14)

---
+++ app/src/main/java/com/github/plplmax/mrv/domain/models/Character.java
@@ -0,0 +1,40 @@
+package com.github.plplmax.mrv.domain.models;
+
+public class Character {
+    private int id;
+    private String name;
+    private String description;
+    private Image thumbnail;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Image getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(Image thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
---
+++ app/src/main/java/com/github/plplmax/mrv/domain/models/Image.java
@@ -0,0 +1,22 @@
+package com.github.plplmax.mrv.domain.models;
+
+public class Image {
+    private String path;
+    private String extension;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+}
