fe4a0dd9dd8acefb366e9edb38478b756869505f
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2021-12-17 12:37:22 +0300

    MRV-014: Create CharactersRepository

---
+++ app/src/main/java/com/github/plplmax/mrv/domain/repository/CharactersRepository.java
@@ -0,0 +1,7 @@
+package com.github.plplmax.mrv.domain.repository;
+
+import com.github.plplmax.mrv.domain.models.FetchCharactersResult;
+
+public interface CharactersRepository {
+    FetchCharactersResult fetchCharacters();
+}
