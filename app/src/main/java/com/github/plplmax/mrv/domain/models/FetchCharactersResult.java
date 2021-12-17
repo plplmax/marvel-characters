198409e8cd09cca6f429823fea680c6396491eb9
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2021-12-17 12:33:37 +0300

    MRR-005: Refactor Result classes

---
+++ app/src/main/java/com/github/plplmax/mrv/domain/models/FetchCharactersResult.java
@@ -0,0 +1,29 @@
+package com.github.plplmax.mrv.domain.models;
+
+import java.util.List;
+
+public interface FetchCharactersResult {
+    class Success implements FetchCharactersResult {
+        private final List<Character> data;
+
+        public Success(List<Character> data) {
+            this.data = data;
+        }
+
+        public List<Character> getData() {
+            return data;
+        }
+    }
+
+    class Fail implements FetchCharactersResult {
+        private final Exception exception;
+
+        public Fail(Exception exception) {
+            this.exception = exception;
+        }
+
+        public Exception getException() {
+            return exception;
+        }
+    }
+}
