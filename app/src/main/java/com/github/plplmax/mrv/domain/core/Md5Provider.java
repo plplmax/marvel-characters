2f64d5488494189d5e1fa968b8ab8f12f08eb4aa
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2021-12-24 10:17:29 +0300

    MRV-031: Create md5 provider

---
+++ app/src/main/java/com/github/plplmax/mrv/domain/core/Md5Provider.java
@@ -0,0 +1,31 @@
+package com.github.plplmax.mrv.domain.core;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+public class Md5Provider {
+    private static final String ALGORITHM = "MD5";
+    private static final byte HEX_LENGTH = 32;
+    private static final short BYTE_BITMASK = 0xFF;
+
+    public static String getMd5(String data) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(ALGORITHM);
+            byte[] dataBytes = data.getBytes(StandardCharsets.UTF_8);
+            messageDigest.update(dataBytes);
+            byte[] hash = messageDigest.digest();
+            StringBuilder builder = new StringBuilder(HEX_LENGTH);
+
+            for (byte b : hash) {
+                int unsignedByte = (b & BYTE_BITMASK);
+                String hexByte = Integer.toHexString(unsignedByte);
+                if (hexByte.length() < 2) builder.append('0');
+                builder.append(hexByte);
+            }
+
+            return builder.toString();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+}
