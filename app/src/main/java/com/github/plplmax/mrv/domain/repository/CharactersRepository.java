7da5315abbf72267e47d9741eff116c160323e98
plplmax <50287455+plplmax@users.noreply.github.com>
2021-12-31 19:25:33 +0300

    MRV-046: Add a cache in the repository

--- app/src/main/java/com/github/plplmax/mrv/domain/repository/CharactersRepository.java
+++ app/src/main/java/com/github/plplmax/mrv/domain/repository/CharactersRepository.java
@@ -1,7 +1,12 @@
 package com.github.plplmax.mrv.domain.repository;
 
+import com.github.plplmax.mrv.domain.models.Character;
 import com.github.plplmax.mrv.domain.models.FetchCharactersResult;
+
+import java.util.List;
 
 public interface CharactersRepository {
     FetchCharactersResult fetchCharacters();
+
+    void saveCharacters(List<Character> characters);
 }
