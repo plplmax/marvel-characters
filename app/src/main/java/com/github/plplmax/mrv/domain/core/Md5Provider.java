e9e2deb318d751fa650fe70fb1e57f326bc27d4e
plplmax <50287455+plplmax@users.noreply.github.com>
2022-01-01 02:37:36 +0300

    MRR-010: Create Md5Provider interface (#29)
    * MRR-010: Create Md5Provider interface
    
    * MRR-010: Create a Dagger provide method
    
    * MRR-010: Add Md5Provider as a parameter

--- app/src/main/java/com/github/plplmax/mrv/domain/core/Md5Provider.java
+++ app/src/main/java/com/github/plplmax/mrv/domain/core/Md5Provider.java
@@ -2,30 +2,39 @@ package com.github.plplmax.mrv.domain.core;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-public class Md5Provider {
-    private static final String ALGORITHM = "MD5";
-    private static final byte HEX_LENGTH = 32;
-    private static final short BYTE_BITMASK = 0xFF;
+public interface Md5Provider {
+    String getMd5(String data);
 
-    public static String getMd5(String data) {
-        try {
-            MessageDigest messageDigest = MessageDigest.getInstance(ALGORITHM);
-            byte[] dataBytes = data.getBytes(StandardCharsets.UTF_8);
-            messageDigest.update(dataBytes);
-            byte[] hash = messageDigest.digest();
-            StringBuilder builder = new StringBuilder(HEX_LENGTH);
+    class Base implements Md5Provider {
+        private static final String ALGORITHM = "MD5";
+        private static final byte HEX_LENGTH = 32;
+        private static final short BYTE_BITMASK = 0xFF;
 
-            for (byte b : hash) {
-                int unsignedByte = (b & BYTE_BITMASK);
-                String hexByte = Integer.toHexString(unsignedByte);
-                if (hexByte.length() < 2) builder.append('0');
-                builder.append(hexByte);
+        @Override
+        public String getMd5(String data) {
+            try {
+                MessageDigest messageDigest = MessageDigest.getInstance(ALGORITHM);
+
+                byte[] dataBytes = data.getBytes(StandardCharsets.UTF_8);
+                messageDigest.update(dataBytes);
+                byte[] hash = messageDigest.digest();
+
+                StringBuilder builder = new StringBuilder(HEX_LENGTH);
+
+                for (byte b : hash) {
+                    int unsignedByte = (b & BYTE_BITMASK);
+                    String hexByte = Integer.toHexString(unsignedByte);
+                    
+                    if (hexByte.length() < 2) builder.append('0');
+                    builder.append(hexByte);
+                }
+
+                return builder.toString();
+            } catch (NoSuchAlgorithmException e) {
+                return "";
             }
-
-            return builder.toString();
-        } catch (Exception e) {
-            return "";
         }
     }
 }
