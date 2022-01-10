16b5667187439a9e65f2c6e494b0e39f63ce38f2
plplmax <50287455+plplmax@users.noreply.github.com>
2022-01-10 18:04:54 +0300

    MRV-063: Add a viewType

--- app/src/main/java/com/github/plplmax/mrv/ui/CharactersAdapter.java
+++ app/src/main/java/com/github/plplmax/mrv/ui/CharactersAdapter.java
@@ -1,29 +1,37 @@
 package com.github.plplmax.mrv.ui;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.RequestBuilder;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.github.plplmax.mrv.R;
 import com.github.plplmax.mrv.domain.models.Character;
-import com.github.plplmax.mrv.domain.models.Image;
 
 import java.util.List;
 
-public class CharactersAdapter extends RecyclerView.Adapter<CharactersAdapter.ViewHolder> {
+public class CharactersAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final byte DATA_VIEW_TYPE = 1;
+    private static final byte FOOTER_VIEW_TYPE = 2;
+
     private final CharactersFragment.OnCharacterClickListener onClickListener;
     private final LayoutInflater inflater;
     private final List<Character> characters;
+
+    private State state = State.LOADING;
+
+    enum State {
+        DONE,
+        LOADING,
+        ERROR
+    }
 
     public CharactersAdapter(Context context,
                              CharactersFragment.OnCharacterClickListener onClickListener,
@@ -35,42 +43,83 @@ public class CharactersAdapter extends RecyclerView.Adapter<CharactersAdapter.Vi
 
     @NonNull
     @Override
-    public CharactersAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.character_item, parent, false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == DATA_VIEW_TYPE) {
+            View view = inflater.inflate(R.layout.character_item, parent, false);
+            return new CharacterViewHolder(view);
+        }
+
+        View view = inflater.inflate(R.layout.loading_item, parent, false);
+        return new FooterViewHolder(view);
     }
 
-    @SuppressLint("CheckResult")
     @Override
-    public void onBindViewHolder(@NonNull CharactersAdapter.ViewHolder holder, int position) {
-        Character character = characters.get(position);
-        Image image = character.getThumbnail();
-        String url = image.toString();
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (getItemViewType(position) == DATA_VIEW_TYPE) {
+            CharacterViewHolder viewHolder = (CharacterViewHolder) holder;
+            viewHolder.bind(position);
+        } else {
+            FooterViewHolder holderFooter = (FooterViewHolder) holder;
+            holderFooter.bind(state);
+        }
+    }
 
-        RequestBuilder<Drawable> requestBuilder = Glide.with(holder.characterView.getContext())
-                .load(url);
-
-        if (image.isAvailable()) requestBuilder.centerCrop();
-
-        requestBuilder.placeholder(R.mipmap.ic_launcher)
-                .transition(DrawableTransitionOptions.withCrossFade())
-                .into(holder.characterView);
-        holder.characterView.setOnClickListener(v -> {
-            onClickListener.onCharacterClick(character);
-        });
+    @Override
+    public int getItemViewType(int position) {
+        if (characters.size() > position) return DATA_VIEW_TYPE;
+        else return FOOTER_VIEW_TYPE;
     }
 
     @Override
     public int getItemCount() {
-        return characters.size();
+        return characters.size() + (hasFooter() ? 1 : 0);
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    private boolean hasFooter() {
+        return characters.size() != 0 && (state == State.LOADING || state == State.ERROR);
+    }
+
+    public class CharacterViewHolder extends RecyclerView.ViewHolder {
         final ImageView characterView;
 
-        public ViewHolder(@NonNull View itemView) {
+        public CharacterViewHolder(@NonNull View itemView) {
             super(itemView);
             characterView = itemView.findViewById(R.id.character_image);
         }
+
+        public void bind(int position) {
+            Character character = characters.get(position);
+            String url = character.getThumbnail().toString();
+
+            Glide.with(characterView.getContext())
+                    .load(url)
+                    .centerInside()
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(characterView);
+
+            characterView.setOnClickListener(v -> onClickListener.onCharacterClick(character));
+        }
+    }
+
+    public static class FooterViewHolder extends RecyclerView.ViewHolder {
+        final ProgressBar progressBar;
+
+        public FooterViewHolder(@NonNull View itemView) {
+            super(itemView);
+            progressBar = itemView.findViewById(R.id.progress_bar);
+        }
+
+        public void bind(State state) {
+            progressBar.setVisibility(state == State.LOADING ? View.VISIBLE : View.INVISIBLE);
+        }
+    }
+
+    public void setState(State state) {
+        this.state = state;
+        notifyItemChanged(getItemCount());
+    }
+
+    public boolean isStateLoading() {
+        return state == State.LOADING;
     }
 }
