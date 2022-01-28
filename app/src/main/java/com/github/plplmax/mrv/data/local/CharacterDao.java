6b5545a7726d278e1b0f2a46ed4ff28f89e705f8
plplmax <50287455+plplmax@users.noreply.github.com>
2022-01-28 10:19:20 +0300

    Fix: Fix bug with a scroll position

--- app/src/main/java/com/github/plplmax/mrv/data/local/CharacterDao.java
+++ app/src/main/java/com/github/plplmax/mrv/data/local/CharacterDao.java
@@ -9,10 +9,10 @@ import java.util.List;
 
 @Dao
 public interface CharacterDao {
-    @Query("SELECT * FROM characters LIMIT 20 OFFSET :offset")
+    @Query("SELECT * FROM characters ORDER BY name LIMIT 20 OFFSET :offset")
     List<CharacterEntity> fetchCharactersWithOffset(int offset);
 
-    @Query("SELECT * FROM characters LIMIT :limit")
+    @Query("SELECT * FROM characters ORDER BY name LIMIT :limit")
     List<CharacterEntity> fetchCharactersWithLimit(int limit);
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
