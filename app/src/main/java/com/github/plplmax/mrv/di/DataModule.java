b74449df606f13afa999ab9d81f70b218310fe50
plplmax <50287455+plplmax@users.noreply.github.com>
2022-02-04 21:28:40 +0300

    Refactor: Rename a provide method

--- app/src/main/java/com/github/plplmax/mrv/di/DataModule.java
+++ app/src/main/java/com/github/plplmax/mrv/di/DataModule.java
@@ -127,7 +127,7 @@ public class DataModule {
 
     @Provides
     @Singleton
-    CharacterResponseMapper provideCharacterDataWrapperResponseMapper() {
+    CharacterResponseMapper provideCharacterResponseMapper() {
         return new CharacterResponseMapper();
     }
 
